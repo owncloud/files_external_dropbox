864d0fd66dad94736dec3d26d5bbfc2ddedac567
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-08 01:22:29 +0000

    [tx-robot] updated from transifex

---
+++ l10n/sl.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Napaka overjanja kode OAuth2 za",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "2. korak je spodletel. Izjemna napaka: %s",
+    "Invalid Request Params!!" : "Neveljavne zahteve parametrov!"
+},
+"nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);");
