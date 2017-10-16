cb830416440b4df2e324d65958d8063b618c5049
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-16 01:22:27 +0000

    [tx-robot] updated from transifex

---
+++ l10n/th_TH.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "เกิดข้อผิดพลาดในการยืนยันรหัส OAuth2 สำหรับ",
+    "Dropbox V2" : "Dropbox รุ่น 2",
+    "Step 2 failed. Exception: %s" : "ขั้นตอนที่ 2 ล้มเหลว ข้อยกเว้น: %s",
+    "Invalid Request Params!!" : "คำร้องขอไม่ถูกต้อง !!"
+},
+"nplurals=1; plural=0;");
