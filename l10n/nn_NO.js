63797faff065149ef1544ce413b30afd4c18d2e4
ownCloud Jenkins <jenkins@owncloud.com>
2017-11-20 01:25:32 +0000

    [tx-robot] updated from transifex

---
+++ l10n/nn_NO.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Feil ved verifisering av OAuth2 kode for",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Steg 2 feila. Feil: %s",
+    "Invalid Request Params!!" : "Ikkje gjeldande parametre for førespurnad!!"
+},
+"nplurals=2; plural=(n != 1);");
