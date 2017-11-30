40de4250ba59ced7b1bc6361be6185c3881ef6ec
ownCloud Jenkins <jenkins@owncloud.com>
2017-11-30 01:33:06 +0000

    [tx-robot] updated from transifex

---
+++ l10n/sq.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Gabim në verifikim Kodi OAuth2 për",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Hapi 2 dÃ«shtoi. PÃ«rjashtim: %s",
+    "Invalid Request Params!!" : "Parametra të Pavlefshme Kërkese!"
+},
+"nplurals=2; plural=(n != 1);");
