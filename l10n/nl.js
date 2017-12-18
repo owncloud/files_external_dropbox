fbe96faaa7fb627e478b6cd6b9497802cee3fc54
ownCloud Jenkins <jenkins@owncloud.com>
2017-12-18 03:14:05 +0000

    [tx-robot] updated from transifex

---
+++ l10n/nl.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Fout bij verifiëren OAuth2 Code voor ",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Stap 2 is mislukt. Uitzondering: %s"
+},
+"nplurals=2; plural=(n != 1);");
