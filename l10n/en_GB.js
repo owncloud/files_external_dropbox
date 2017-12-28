3cfc13d9bbba8020cc39a4eaa06767d9ae78716b
ownCloud Jenkins <jenkins@owncloud.com>
2017-12-28 03:14:24 +0000

    [tx-robot] updated from transifex

--- l10n/en_GB.js
+++ l10n/en_GB.js
@@ -1,10 +1,9 @@
 OC.L10N.register(
     "files_external_dropbox",
     {
-        "Dropbox" : "Dropbox",
-        "Client Id" : "Client Id",
-        "Client Secret" : "Client Secret",
-        "Token" : "Token"
-    },
-    "nplurals=2; plural=(n != 1);"
-);
+    "Error verifying OAuth2 Code for " : "Error verifying OAuth2 Code for ",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Step 2 failed. Exception: %s",
+    "Invalid Request Params!!" : "Invalid Request Params!!"
+},
+"nplurals=2; plural=(n != 1);");
