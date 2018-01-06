a526c647e29fc887b97849e3f5dabd776855c8fa
ownCloud Jenkins <jenkins@owncloud.com>
2018-01-06 01:23:36 +0000

    [tx-robot] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "다음의 OAuth2 코드 검증 오류",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "2단계 실패. 예외: %s",
+    "Invalid Request Params!!" : "잘못된 요청 인자!!"
+},
+"nplurals=1; plural=0;");
