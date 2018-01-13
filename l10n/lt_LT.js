60396b3f95001f2e4b1c829ba80d565b2d2b310a
ownCloud Jenkins <jenkins@owncloud.com>
2018-01-13 03:16:33 +0000

    [tx-robot] updated from transifex

---
+++ l10n/lt_LT.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Klaida, patvirtinant OAuth2 kodą, skirtą  ",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "2 žingsnio klaida: %s",
+    "Invalid Request Params!!" : "Negalimi užklausos parametrai!"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);");
