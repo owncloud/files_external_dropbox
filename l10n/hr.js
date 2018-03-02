10d674c56e17cf3bff87d1c4b3d1cf1dc3a7c426
ownCloud Jenkins <jenkins@owncloud.com>
2018-03-02 01:26:24 +0000

    [tx-robot] updated from transifex

---
+++ l10n/hr.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Greška provjere OAuth2 koda za",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Korak 2 nije uspio. Izuzetak: %s",
+    "Invalid Request Params!!" : "Parametri zahtjeva neispravni!!"
+},
+"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;");
--- l10n/is.js
+++ l10n/is.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "files_external_dropbox",
     {
-    "Error verifying OAuth2 Code for " : "Villa við að sannvotta OAuth2-kóða fyrir",
+    "Error verifying OAuth2 Code for " : "Villa við að sannvotta OAuth2-kóða fyrir ",
     "Dropbox V2" : "Dropbox V2",
     "Step 2 failed. Exception: %s" : "Skref 2 mistókst. Undantekning: %s",
     "Invalid Request Params!!" : "Ógild viðföng í fyrirspurn!!"
