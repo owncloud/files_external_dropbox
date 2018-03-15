d0834d9698e71dd73e7fc08001bed8779c71dc64
ownCloud Jenkins <jenkins@owncloud.com>
2018-03-15 01:22:54 +0000

    [tx-robot] updated from transifex

---
+++ l10n/ca.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Error verificant el codi OAuth2 ",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "El pas 2 ha fallat. Excepció: %s",
+    "Invalid Request Params!!" : "Paràmetres invàlids!!"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/lo.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "ຂໍ້ຜິດພາດຢືນຢັນລະຫັດ OAuth2 ສໍາລັບ",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "ຂັ້ນຕອນທີ 2 ລົ້ມເຫລວ. ຂໍ້​ຍົກ​ເວັ້ນ: %s",
+    "Invalid Request Params!!" : "ຄໍາຮ້ອງຂໍບໍ່ຖືກຕ້ອງ Params!!"
+},
+"nplurals=1; plural=0;");
