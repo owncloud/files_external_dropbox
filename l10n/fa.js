28f75122d1f20dcd92cb041c6e68ea5497e9ec0c
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-17 01:22:21 +0000

    [tx-robot] updated from transifex

--- l10n/fa.js
+++ l10n/fa.js
@@ -6,4 +6,4 @@ OC.L10N.register(
     "Step 2 failed. Exception: %s" : "گام ۲ شکست خورد. خطا: %s",
     "Invalid Request Params!!" : "پارامترهای درخواست نامعتبر!!"
 },
-"nplurals=1; plural=0;");
+"nplurals=2; plural=((n<=1 && n>= 0) ? 1);");
--- l10n/he.js
+++ l10n/he.js
@@ -6,4 +6,4 @@ OC.L10N.register(
     "Step 2 failed. Exception: %s" : "שלב 2 נכשל. חריג: %s",
     "Invalid Request Params!!" : "בקשת Params לא חוקית!!"
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=4; plural=(n==1 ? 1: n == 2 ? 2 : (n % 10 == 0 and n > 10) ? 4);");
