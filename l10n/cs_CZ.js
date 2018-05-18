e99d9605ae36b924c1823b1e3731496179a96875
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-18 01:22:21 +0000

    [tx-robot] updated from transifex

--- l10n/cs_CZ.js
+++ l10n/cs_CZ.js
@@ -6,4 +6,4 @@ OC.L10N.register(
     "Step 2 failed. Exception: %s" : "Selhal krok 2. Výjimka: %s",
     "Invalid Request Params!!" : "Chybné parametry požadavku!!"
 },
-"nplurals=4; plural=(n < 10 && n % 1 == 0) ? 1 : (n < 9999 && n >= 10 && n % 1 == 0) ? 3 : (n % 1 != 0) : 4;");
+"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n >= 2 && n <= 4 && n % 1 == 0) ? 1: (n % 1 != 0 ) ? 2 : 3;");
