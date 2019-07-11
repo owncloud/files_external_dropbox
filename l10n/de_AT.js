43e6a12e05774007592c536f4a6abe5f115181dd
ownClouders <devops@owncloud.com>
2019-07-11 05:01:43 +0000

    [tx] updated from transifex

---
+++ l10n/de_AT.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Bei der Verifikation des OAuth2 Codes für [ ] ist ein Fehler aufgetreten.",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Schritt 2 ist fehlgeschlagen. Ausnahme: %s",
+    "Invalid Request Params!!" : "Ungültiger Anfrageparameter!!"
+},
+"nplurals=2; plural=(n != 1);");
