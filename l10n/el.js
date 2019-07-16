c155f74f35cb5538e6c6a2f7c11cd1661ff59a46
ownClouders <devops@owncloud.com>
2019-07-16 05:01:06 +0000

    [tx] updated from transifex

--- l10n/el.js
+++ l10n/el.js
@@ -1,8 +1,9 @@
 OC.L10N.register(
     "files_external_dropbox",
     {
-    "Error verifying OAuth2 Code for " : "Σφάλμα κατά την επαλήθευση του κώδικα OAuth2 για",
+    "Error verifying OAuth2 Code for " : "Σφάλμα κατά την επαλήθευση του κώδικα OAuth2 για ",
     "Dropbox V2" : "Dropbox V2",
-    "Step 2 failed. Exception: %s" : "Το βήμα 2 απέτυχε. Εξαίρεση: %s"
+    "Step 2 failed. Exception: %s" : "Το βήμα 2 απέτυχε. Εξαίρεση: %s",
+    "Invalid Request Params!!" : "Μη έγκυρες παράμετροι αιτήματος!"
 },
 "nplurals=2; plural=(n != 1);");
