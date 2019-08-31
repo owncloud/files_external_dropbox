be2d418c26a8bb7676ae331e0518e5b56626768d
ownClouders <devops@owncloud.com>
2019-08-31 01:18:13 +0000

    [tx] updated from transifex

---
+++ l10n/eu.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Errorea gertatu da OAuth2 Kodea egiaztatzean honentzat",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "2 Urratsak huts egin du. Salbuespena: %s",
+    "Invalid Request Params!!" : "Eskakizun parametro baliogabeak!!"
+},
+"nplurals=2; plural=(n != 1);");
