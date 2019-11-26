8bb1665da1d9dccc1face86fe74a68043faed685
Dominik Schmidt <domme@tomahawk-player.org>
2019-11-26 15:03:59 +0000

    [tx] updated from transifex

---
+++ l10n/lv.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Kļūda verificējot OAuth2 kodu priekš ",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Otrais solis neizdevās. Izņēmums: %s"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);");
