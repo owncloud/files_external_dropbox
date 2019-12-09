e2e62aa9cc3acea54c8d287ef20a992c6690da6e
Dominik Schmidt <domme@tomahawk-player.org>
2019-12-09 16:41:59 +0000

    [tx] updated from transifex

---
+++ l10n/tr.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Adım 2 başarısız. İstisna: %s",
+    "Invalid Request Params!!" : "Geçersiz İstek Parametreleri!!"
+},
+"nplurals=2; plural=(n > 1);");
