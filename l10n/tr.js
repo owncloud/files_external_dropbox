ffbbfa1bb389b8cc74814bb4214ddbe2e5c11aa9
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-23 02:27:22 +0000

    [tx] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "files_external_dropbox",
     {
+    "Error verifying OAuth2 Code for " : "OAuth2 Kodu doğrulanırken hata oluştu",
     "Dropbox V2" : "Dropbox V2",
     "Step 2 failed. Exception: %s" : "Adım 2 başarısız. İstisna: %s",
     "Invalid Request Params!!" : "Geçersiz İstek Parametreleri!!"
