e1cfe302539f6b04137594f40a1e17813f0654e3
Thomas Müller <thomas.mueller@tmit.eu>
2020-08-04 01:37:55 +0000

    [tx] updated from transifex

---
+++ l10n/ar.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "خطأ في التحقق من صحة رمز OAuth2 لـ",
+    "Dropbox V2" : "دروبوكس V2",
+    "Step 2 failed. Exception: %s" : "فشلت الخطوة 2. استثناء: %s",
+    "Invalid Request Params!!" : "معلمات طلب غير صالحة!!"
+},
+"nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
