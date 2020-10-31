4acaf8f135c05ee9328a6cb7a2c9851d77725573
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-31 00:05:28 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "OAuth2-Code Überprüfungsfehler bei",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Schritt 2 fehlgeschlagen. Fehlermeldung: %s",
+    "Invalid Request Params!!" : "Ungültige Anfrageparameter!"
+},
+"nplurals=2; plural=(n != 1);");
