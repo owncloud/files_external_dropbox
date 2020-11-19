3a1dd20cdcbb1bc236226b024ba5649e45a857e9
Thomas Müller <thomas.mueller@tmit.eu>
2020-11-19 01:23:40 +0000

    [tx] updated from transifex

---
+++ l10n/hu_HU.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Hiba az OAuth2 kód ellenőrzése miatt",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "2. lépés sikertelen. Hibakód: %s",
+    "Invalid Request Params!!" : "Érvénytelen kérelmi paraméterek!"
+},
+"nplurals=2; plural=(n != 1);");
