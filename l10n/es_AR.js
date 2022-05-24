735d32f1b91ae2806d0a4a9f347a13246c21e469
ownClouders <devops@owncloud.com>
2022-05-24 00:36:12 +0000

    [tx] updated from transifex

--- l10n/es_AR.js
+++ l10n/es_AR.js
@@ -6,4 +6,4 @@ OC.L10N.register(
     "Step 2 failed. Exception: %s" : "Error en paso 2. Excepción: %s",
     "Invalid Request Params!!" : "Parámetros de requerimiento inválidos!!"
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
