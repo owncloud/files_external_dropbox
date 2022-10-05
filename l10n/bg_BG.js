0b25b7435366b90a83d19f4ce1111af1d1c19273
ownClouders <devops@owncloud.com>
2022-10-05 00:44:12 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -1,9 +1,9 @@
 OC.L10N.register(
     "files_external_dropbox",
     {
-    "Error verifying OAuth2 Code for " : "Грешка при проверката на на OAuth2 кодър за",
+    "Error verifying OAuth2 Code for " : "Грешка при проверката на кода OAuth2 за",
     "Dropbox V2" : "Dropbox V2",
-    "Step 2 failed. Exception: %s" : "Стъпка 2 - неуспешна. Грешка: %s",
-    "Invalid Request Params!!" : "Невалидни параметри в заявката!"
+    "Step 2 failed. Exception: %s" : "Стъпка 2 е неуспешна. Изключение: %s",
+    "Invalid Request Params!!" : "Невалидни параметри в заявката!!"
 },
 "nplurals=2; plural=(n != 1);");
