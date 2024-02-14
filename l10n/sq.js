72298167f55b6a17ba523a737ea3e4d94402e0fa
ownClouders <devops@owncloud.com>
2024-02-14 08:11:54 +0000

    [tx] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "files_external_dropbox",
     {
-    "Error verifying OAuth2 Code for " : "Gabim në verifikim Kodi OAuth2 për",
+    "Error verifying OAuth2 Code for " : "Gabim në verifikim Kodi OAuth2 për ",
     "Dropbox V2" : "Dropbox V2",
     "Step 2 failed. Exception: %s" : "Hapi 2 dështoi. Përjashtim: %s",
     "Invalid Request Params!!" : "Parametra të Pavlefshme Kërkese!"
