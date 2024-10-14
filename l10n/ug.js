559f66fd7f3358abb924f50c5f7696a860c57639
ownClouders <devops@owncloud.com>
2024-10-14 01:39:05 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "ئۈچۈن OAuth2 كودىنى دەلىللەشتە خاتالىق",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "ئىككىنچى قەدەم مەغلۇپ بولدى. مۇستەسنا:% s",
+    "Invalid Request Params!!" : "ئىناۋەتسىز تەلەپ پارامېتىرلىرى !!"
+},
+"nplurals=2; plural=(n != 1);");
