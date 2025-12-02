00d4f76e1ef659f676a85a5d7e67f00cac90dc70
ownClouders <devops@owncloud.com>
2025-12-02 00:06:23 +0000

    [tx] updated from transifex

---
+++ l10n/en_US.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "Error verifying OAuth2 Code for ",
+    "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "Step 2 failed. Exception: %s",
+    "Invalid Request Params!!" : "Invalid Request Params!!"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/zh_HK.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_external_dropbox",
+    {
+    "Error verifying OAuth2 Code for " : "驗證 OAuth2 代碼時發生錯誤",
+    "Dropbox V2" : "Dropbox V2",
+    "Invalid Request Params!!" : "请求的参数无效!!"
+},
+"nplurals=1; plural=0;");
