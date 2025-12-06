eb13b078f2ae9d050713788c45e0a887470c73cf
ownClouders <devops@owncloud.com>
2025-12-06 00:18:37 +0000

    [tx] updated from transifex

--- l10n/zh-Hans.js
+++ l10n/zh-Hans.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Error verifying OAuth2 Code for " : "驗證 OAuth2 代碼時發生錯誤",
     "Dropbox V2" : "Dropbox V2",
+    "Step 2 failed. Exception: %s" : "步骤 2 失败。异常：%s",
     "Invalid Request Params!!" : "请求的参数无效!!"
 },
 "nplurals=1; plural=0;");
